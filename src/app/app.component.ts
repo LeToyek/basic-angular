5ba64808f910c767df496bdd468af4625c843a50
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-01-15 21:45:19 +0700

    simple change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'basic-angular';
+
+  count = 0;
+  constructor() {
+
+    const id = setInterval(() => {
+      this.count++;
+    }
+    , 1000);
+  }
+  ngOnDestroy() {
+    clearInterval(this.count);
+  }
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
 }
